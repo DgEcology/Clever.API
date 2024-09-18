fd252f70b8168cb73161241b93cc9e022f0ab3db
Vladimir Arkhipenkov <vova.arxipenkov@bk.ru>
2024-09-18 12:44:16 +0300

    NotFoundException naming fixed

--- src/Clever.Domain/Exceptions/NotFoundException.cs
+++ src/Clever.Domain/Exceptions/NotFoundException.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Clever.Domain.Entities
+namespace Clever.Domain.Exceptions
 {
     public class NotFoundException : Exception
     {
-        public NotFoundException(string entityName, int id) : base($"Entity ({entityName}) with the specified ID ({id}) could not be found") {
-            
-        }
+        public NotFoundException(string entityName, int id) : base($"Entity ({entityName}) with the specified ID ({id}) could not be found") {}
+
     }
 }
