89acfcba177337150a579138ac79b628aef0b51b
Vladimir Arkhipenkov <vova.arxipenkov@bk.ru>
2024-09-18 14:07:16 +0300

    Domain changed

--- src/Clever.Domain/Entities/Event.cs
+++ src/Clever.Domain/Entities/Event.cs
@@ -11,42 +11,42 @@ namespace Clever.Domain.Entities
     public class Event
     {
         [Key]
-        public long Id {get; set;}
+        public long Id { get; set; }
 
-        public string Description {get; set;} 
+        public string Description { get; set; } 
 
-        public string Title {get; set;}
+        public string Title { get; set; }
         
-        public string Image {get; set;}
+        public string Image { get; set; }
 
-        public string Geolocation {get; set;}
+        public string Geolocation { get; set; }
 
-        public DateTime StartTime {get; set;}
+        public DateTime StartTime { get; set; }
 
-        public DateTime EndTime {get; set;}
+        public DateTime EndTime { get; set; }
 
-        public DateTime PublishTime {get; set;}
-        public bool IsArchived {get; set;}
+        public DateTime PublishTime { get; set; }
+        public bool IsArchived { get; set; }
 
-        public string SecretKey {get; set;}
+        public string SecretKey { get; set; }
 
-        public long TagId {get; set;}
+        public long TagId { get; set; }
 
-        public string UserId {get; set;}
+        public string UserId { get; set; }
 
-        public long ReactionId {get; set;}
+        public long ReactionId { get; set; }
 
         public bool IsAccepted { get; set; }
 
-        public long AttendanceId {get; set;}
+        public long AttendanceId { get; set; }
 
-        public Tag? Tag {get; set;}
+        public Tag? Tag { get; set; }
 
-        public User? User {get; set;}
+        public User? User { get; set; }
 
-        public ICollection<Reaction>? Reactions {get; set;}
+        public ICollection<Reaction>? Reactions { get; set; }
 
-        public ICollection<Attendance>? Attendances {get; set;}
+        public ICollection<Attendance>? Attendances { get; set; }
 
     }
 }
--- src/Clever.Domain/Entities/User.cs
+++ src/Clever.Domain/Entities/User.cs
@@ -8,22 +8,20 @@ namespace Clever.Domain.Entities
 {
     public class User : IdentityUser
     {
-        public string AboutMe { get; set; }
+        public string About { get; set; }
 
         public int Points { get; set; }
 
-        public int EventsOrganised { get; set; }
-
-        public string Picture { get; set; }
+        public string ProfilePicture { get; set; }
 
         public string PassportData { get; set; }
 
-        public ICollection<Attendance> Attendances { get; set; }
+        public ICollection<Attendance> Attendance { get; set; }
 
         public ICollection<Event> Events { get; set; }
 
         public ICollection<Reaction> Reactions { get; set; }
 
-        public ICollection<OrganiserApplication> OrganiserApplications { get; set; }
+        public OrganiserApplication OrganiserApplication { get; set; }
     }
 }
