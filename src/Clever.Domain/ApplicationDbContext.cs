f5d63ed0618c02253246ae8e912842373bfe0313
Vladimir Arkhipenkov <vova.arxipenkov@bk.ru>
2024-09-18 12:09:13 +0300

    Dbcontext configuration

--- src/Clever.Domain/ApplicationDbContext.cs
+++ src/Clever.Domain/ApplicationDbContext.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Clever.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace Clever.Domain
 {
@@ -52,6 +55,15 @@ namespace Clever.Domain
             .WithMany(u => u.OrganiserApplications)
             .HasForeignKey(o => o.UserId);
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("dbsettings.json")
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
+            optionsBuilder.UseNpgsql(config.GetConnectionString("DefaultConnection"));
         }
     }
 }
