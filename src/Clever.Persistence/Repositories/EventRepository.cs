b0184abb77035b32e73c4b6f3fe72b116ff6b000
Vladimir Arkhipenkov <vova.arxipenkov@bk.ru>
2024-09-18 13:21:05 +0300

    EventRepository added

---
+++ src/Clever.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Clever.Domain.Interfaces;
+using Clever.Domain.Entities;
+using Clever.Domain;
+using Clever.Domain.Exceptions;
+
+namespace Clever.Persistence.Repositories;
+public class EventRepository : IEventRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public EventRepository(ApplicationDbContext applicationDbContext) => _applicationDbContext = applicationDbContext;
+
+    public async Task<List<Event>> GetAllAsync() => await _applicationDbContext.Events.ToListAsync();
+
+    public async Task<Event> GetByIdAsync(long id)
+    {
+        var ev = await _applicationDbContext.Events.FirstOrDefaultAsync(e => e.Id == id);
+
+        if(ev is null){
+            throw new NotFoundException(typeof(Event).Name, id);
+        }
+
+        return ev;
+    }
+    public void Add(Event entity)
+    {
+        _applicationDbContext.Events.Add(entity);
+    }
+
+    public async Task Archive(long id)
+    {
+        var ev = await _applicationDbContext.Events.FirstOrDefaultAsync(x => x.Id == id);
+
+        if( ev is null){
+            throw new NotFoundException(typeof(Event).Name, id);
+        }
+
+        ev.IsArchived = true;
+    }
+
+}
